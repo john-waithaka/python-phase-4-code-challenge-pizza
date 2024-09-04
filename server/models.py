cba5619a5f249a250a82b03c2391986d7750f6fd
John Warui Waithaka <johnwaruiwaithaka@gmail.com>
2024-09-04 23:42:23 +0300

    sort off

--- server/models.py
+++ server/models.py
@@ -1,3 +1,67 @@
+# from flask_sqlalchemy import SQLAlchemy
+# from sqlalchemy import MetaData
+# from sqlalchemy.orm import validates
+# from sqlalchemy.ext.associationproxy import association_proxy
+# from sqlalchemy_serializer import SerializerMixin
+
+# metadata = MetaData(
+#     naming_convention={
+#         "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+#     }
+# )
+
+# db = SQLAlchemy(metadata=metadata)
+
+
+# class Restaurant(db.Model, SerializerMixin):
+#     __tablename__ = "restaurants"
+
+#     id = db.Column(db.Integer, primary_key=True)
+#     name = db.Column(db.String)
+#     address = db.Column(db.String)
+
+#     # add relationship
+
+#     # add serialization rules
+
+#     def __repr__(self):
+#         return f"<Restaurant {self.name}>"
+
+
+# class Pizza(db.Model, SerializerMixin):
+#     __tablename__ = "pizzas"
+
+#     id = db.Column(db.Integer, primary_key=True)
+#     name = db.Column(db.String)
+#     ingredients = db.Column(db.String)
+
+#     # add relationship
+
+#     # add serialization rules
+
+#     def __repr__(self):
+#         return f"<Pizza {self.name}, {self.ingredients}>"
+
+
+# class RestaurantPizza(db.Model, SerializerMixin):
+#     __tablename__ = "restaurant_pizzas"
+
+#     id = db.Column(db.Integer, primary_key=True)
+#     price = db.Column(db.Integer, nullable=False)
+
+#     # add relationships
+
+#     # add serialization rules
+
+#     # add validation
+
+#     def __repr__(self):
+#         return f"<RestaurantPizza ${self.price}>"
+
+
+
+
+
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
 from sqlalchemy.orm import validates
@@ -21,8 +85,12 @@ class Restaurant(db.Model, SerializerMixin):
     address = db.Column(db.String)
 
     # add relationship
+    restaurant_pizzas = db.relationship('RestaurantPizza', back_populates='restaurant',cascade='delete')
+    pizzas = association_proxy('restaurant_pizzas', 'pizza',
+        creator=lambda pizza_obj: RestaurantPizza(pizza=pizza_obj))
 
     # add serialization rules
+    serialize_rules = ('-restaurant_pizzas.restaurant', '-pizzas.restaurants',)
 
     def __repr__(self):
         return f"<Restaurant {self.name}>"
@@ -36,8 +104,12 @@ class Pizza(db.Model, SerializerMixin):
     ingredients = db.Column(db.String)
 
     # add relationship
-
-    # add serialization rules
+    # Relationship mapping the pizzas to related restaurant_pizzas
+    restaurant_pizzas = db.relationship('RestaurantPizza', back_populates='pizza',cascade='delete')
+    restaurants = association_proxy('restaurant_pizzas', 'pizza',
+        creator=lambda restaurant_obj: RestaurantPizza(restaurant=restaurant_obj))
+    
+    serialize_rules = ('-restaurant_pizzas.pizza', '-restaurants.pizzas')
 
     def __repr__(self):
         return f"<Pizza {self.name}, {self.ingredients}>"
@@ -50,10 +122,26 @@ class RestaurantPizza(db.Model, SerializerMixin):
     price = db.Column(db.Integer, nullable=False)
 
     # add relationships
+    # Foreign key to store the restaurant id
+    restaurant_id = db.Column(db.Integer, db.ForeignKey('restaurants.id'))
+    # Foreign key to store the pizza id
+    pizza_id = db.Column(db.Integer, db.ForeignKey('pizzas.id'))
+
+    # Relationship mapping the assignment to related restaurant
+    restaurant = db.relationship('Restaurant', back_populates='restaurant_pizzas')
+    # Relationship mapping the assignment to related pizza
+    pizza = db.relationship('Pizza', back_populates='restaurant_pizzas')
 
     # add serialization rules
+    #serialize_rules = ('-pizzas.restaurant_pizzas', '-restaurants.restaurant_pizzas')
+    serialize_rules = ('-restaurant.restaurant_pizzas', '-pizza.restaurant_pizzas')
 
     # add validation
+    @validates('price')
+    def validate_price(self, key, price):
+        if not (1 <= price <= 30):
+            raise ValueError("Price must be between 1 and 30")
+        return price
 
     def __repr__(self):
         return f"<RestaurantPizza ${self.price}>"
